58fb78c0461f780e954cc320c9845c098749aac3
Xu Fu Ou <xufuou@gmail.com>
2016-04-04 16:12:12 +0100

    add mrmr method

---
+++ src/mrmr/mrmr_mid_d.m
@@ -0,0 +1,56 @@
+function [fea, vals] = mrmr_mid_d(d, f, K)
+% function [fea] = mrmr_mid_d(d, f, K)
+% 
+% MID scheme according to MRMR
+%
+% By Hanchuan Peng
+% April 16, 2003
+%
+
+bdisp=0;
+
+nd = size(d,2);
+nc = size(d,1);
+
+t1=cputime;
+for i=1:nd, 
+   t(i) = mutualinfo(d(:,i), f);
+end; 
+
+[tmp, idxs] = sort(-t);
+fea_base = idxs(1:K);
+vals(1) = -tmp(1);
+fea(1) = idxs(1);
+
+KMAX = min(1000,nd); %500
+
+idxleft = idxs(2:KMAX);
+
+k=1;
+
+for k=2:K,
+   t1=cputime;
+   ncand = length(idxleft);
+   curlastfea = length(fea);
+   for i=1:ncand,
+      t_mi(i) = mutualinfo(d(:,idxleft(i)), f); 
+      mi_array(idxleft(i),curlastfea) = getmultimi(d(:,fea(curlastfea)), d(:,idxleft(i)));
+      c_mi(i) = mean(mi_array(idxleft(i), :)); 
+   end;
+
+   [tmp, fea(k)] = max(t_mi(1:ncand) - c_mi(1:ncand));
+
+   tmpidx = fea(k); fea(k) = idxleft(tmpidx); idxleft(tmpidx) = [];
+
+   vals(k) = tmp;
+
+end;
+
+return;
+
+%===================================== 
+function c = getmultimi(da, dt) 
+for i=1:size(da,2), 
+   c(i) = mutualinfo(da(:,i), dt);
+end; 
+    
